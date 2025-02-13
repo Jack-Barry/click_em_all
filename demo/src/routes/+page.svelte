5dbbbe17fa7062845250bc85f6a01912fd18a9b1
Jack Barry <Jack@Barry.onl>
2025-02-12 19:02:49 -0700

    chore: Add a demo page for running sequences

---
+++ demo/src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	let itemCount = $state(0);
+	let items = $derived(Array.from({ length: itemCount }).map((_, index) => index + 1));
+
+	let itemClickCount = $state(0);
+	let addMoreClickCount = $state(0);
+</script>
+
+<button
+	onclick={() => {
+		itemCount = 0;
+		itemClickCount = 0;
+		addMoreClickCount = 0;
+	}}
+>
+	Reset
+</button>
+
+<p>Add more clicked {addMoreClickCount} times</p>
+<button
+	id="add-more-button"
+	onclick={() => {
+		itemCount += 10;
+		addMoreClickCount++;
+	}}
+>
+	Add more items
+</button>
+
+<p>Items clicked {itemClickCount} times</p>
+{#each items as item}
+	<div>
+		<button
+			class="item-button"
+			onclick={() => {
+				itemClickCount++;
+			}}
+		>
+			Item #{item}
+		</button>
+	</div>
+{/each}
